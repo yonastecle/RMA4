161fe72085e0d4ac37489a94cdcc1d3f960e8dc1
Tanmeet Kaur <tanmeet@cnbcomputers.com>
2016-07-08 15:14:27 -0400

    Serach Button Enabled

--- SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/WO_Details.Designer.cs
+++ SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/WO_Details.Designer.cs
@@ -29,22 +29,45 @@
         private void InitializeComponent()
         {
             this.dataGridView_WODetails = new System.Windows.Forms.DataGridView();
+            this.textBox_rmaNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.backButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_WODetails)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView_WODetails
             // 
             this.dataGridView_WODetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView_WODetails.Location = new System.Drawing.Point(0, 33);
+            this.dataGridView_WODetails.Location = new System.Drawing.Point(2, 65);
             this.dataGridView_WODetails.Name = "dataGridView_WODetails";
-            this.dataGridView_WODetails.Size = new System.Drawing.Size(1012, 391);
+            this.dataGridView_WODetails.Size = new System.Drawing.Size(1010, 359);
             this.dataGridView_WODetails.TabIndex = 0;
+            // 
+            // textBox_rmaNo
+            // 
+            this.textBox_rmaNo.Location = new System.Drawing.Point(156, 21);
+            this.textBox_rmaNo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBox_rmaNo.Multiline = true;
+            this.textBox_rmaNo.Name = "textBox_rmaNo";
+            this.textBox_rmaNo.Size = new System.Drawing.Size(255, 23);
+            this.textBox_rmaNo.TabIndex = 39;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(52, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Enter RMA#:";
             // 
             // backButton
             // 
             this.backButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backButton.Location = new System.Drawing.Point(926, 4);
+            this.backButton.Location = new System.Drawing.Point(926, 19);
             this.backButton.Name = "backButton";
             this.backButton.Size = new System.Drawing.Size(75, 23);
             this.backButton.TabIndex = 13;
@@ -52,23 +75,41 @@
             this.backButton.UseVisualStyleBackColor = true;
             this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
+            // SearchButton
+            // 
+            this.SearchButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchButton.Location = new System.Drawing.Point(435, 19);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(76, 25);
+            this.SearchButton.TabIndex = 40;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
             // WO_Details
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
             this.ClientSize = new System.Drawing.Size(1013, 424);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.textBox_rmaNo);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.backButton);
             this.Controls.Add(this.dataGridView_WODetails);
             this.Name = "WO_Details";
             this.Text = "WO_Details";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView_WODetails)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button backButton;
         public System.Windows.Forms.DataGridView dataGridView_WODetails;
+        private System.Windows.Forms.TextBox textBox_rmaNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button SearchButton;
     }
 }
--- SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/WO_Details.cs
+++ SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/WO_Details.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace RMA_SystemSoftware
 {
     public partial class WO_Details : Form
     {
-      
+        SqlConnection con = new SqlConnection(@"Data Source=NimeshPatel-RMA\SQLEXPRESS;Initial Catalog=RMA_System;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        DataSet ds;
+
+
         public WO_Details()
         {
             InitializeComponent();
@@ -20,10 +26,48 @@ namespace RMA_SystemSoftware
 
         private void backButton_Click(object sender, EventArgs e)
         {
-            this.Close();
             Supervisor sup = new Supervisor();
+            this.Close();
             sup.Show();
             
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+               if (textBox_rmaNo.Text != "")
+                {
+                    string result = null;
+                    if (con.State == ConnectionState.Open) con.Close();
+                    con.Open();
+                    cmd = new SqlCommand("Select count(1) found from RMA where rma_no=@RMA", con);
+                    cmd.Parameters.AddWithValue("@RMA", textBox_rmaNo.Text);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                        result = String.Format("{0}", reader["found"]);
+                    con.Close();
+                    if (result.Equals("0"))
+                        MessageBox.Show("RMA not found. Enter a Valid RMA#");
+                    else
+                    {
+                        con.Open();
+                        da = new SqlDataAdapter("Select rma_no as 'RMA #',customer as 'Client Name',userID as ' Tech Assigned',invoiceNo as 'Invoice No.',Status as 'Current Status',type as' Request Type',quantity,ups as 'UPS#',mar as 'MAR',orderNo,serialNo,date_received as ' Received On',date_assigned as'Assigned On',date_hold as 'Put on Hold since',date_wait as ' Waiting since',date_completed as ' Completed On',date_closed as 'closed on' from RMA where rma_no='" + textBox_rmaNo.Text + "'", con);
+                        ds = new DataSet();
+                        da.Fill(ds, "Searched Record");
+                        dataGridView_WODetails.DataSource = ds.Tables["Searched Record"];
+                        con.Close();
+                    }
+                }
+                else
+                    MessageBox.Show("Please Enter RMA No. ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
         }
     }
 }
