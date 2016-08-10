2f20932ba57551846396a31999889642b39dac7b
Tanmeet Kaur <tanmeet@cnbcomputers.com>
2016-08-10 10:33:01 -0400

    Notes combined View Completed!

--- SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/Notes.cs
+++ SourceCode/RMA_SystemSoftware/RMA_SystemSoftware/Notes.cs
@@ -85,7 +85,8 @@ namespace RMA_SystemSoftware
         }
 
         public void History(RMA rma, ref TextBox notes, ref Label rmaNum, ref Label status, ref Label type, ref Label invoice, ref Label order, ref Label serial)
-        {           
+        {
+            string Desp = "", Res = "", Stat = "", Comm = "";     
             try
             {
                 con.Open();
@@ -93,52 +94,37 @@ namespace RMA_SystemSoftware
                 read = cmd.ExecuteReader();
                 while (read.Read())
                 {
-                    string abc = "";
                     rmaNum.Text = read.GetString(read.GetOrdinal("rma_no"));
                     status.Text = read.GetString(read.GetOrdinal("Status"));                   
                     type.Text = read.GetString(read.GetOrdinal("type"));
                     invoice.Text = Convert.ToString(read["invoiceNo"]);
                     order.Text = Convert.ToString(read["orderNo"]);
                     serial.Text = Convert.ToString(read["serialNo"]);
-                }
-           
+                }           
                 con.Close();
+
+                con.Open();
+                cmd = new SqlCommand("Select * from Notes where RMA_no='" + rma.rma_no + "'", con);
+                read = cmd.ExecuteReader();
+                while (read.Read())
+                {
+                    Desp = read.GetString(read.GetOrdinal("description"));
+                    Res = read.GetString(read.GetOrdinal("resolution"));
+                    Stat = read.GetString(read.GetOrdinal("statusUpdates"));
+                    Comm = read.GetString(read.GetOrdinal("comments"));                   
+                }
+                con.Close();
+                //notes.Clear();
+                notes.Text = " \b Description"+ System.Environment.NewLine + Desp + System.Environment.NewLine + "___________________________________________________________________________________________________________" + System.Environment.NewLine;                    
+                notes.Text += " \b Resolution" + System.Environment.NewLine + Res + System.Environment.NewLine + "___________________________________________________________________________________________________________" + System.Environment.NewLine;
+                notes.Text += " \b Status Updates" + System.Environment.NewLine + Stat + System.Environment.NewLine + "___________________________________________________________________________________________________________" + System.Environment.NewLine;
+                notes.Text += " \b Comments" + System.Environment.NewLine + Comm + System.Environment.NewLine + "___________________________________________________________________________________________________________";
+               
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-            }
-
-           // string Desp = "", Res = "", Stat = "", Comm = "";
-            cmd = new SqlCommand("Select * from Notes where RMA_no='"+ rma.rma_no+"'", con);
-            Console.WriteLine(rma.rma_no + " hi there");
-            //cmd.Parameters.AddWithValue("@rmaNum", rma.rma_no);
-
-            //try
-            //{
-                
-
-            //    if (con.State == ConnectionState.Open) con.Close();
-
-            //    con.Open();
-            //    read = cmd.ExecuteReader();
-            //    while (read.Read())
-            //    {
-            //        Desp = read.GetString(read.GetOrdinal(description));
-            //        Res = read.GetString(read.GetOrdinal(resolution));
-            //        Stat = read.GetString(read.GetOrdinal(statusUpdates));
-            //        Comm = read.GetString(read.GetOrdinal(comments));
-            //        Console.WriteLine("reading data");
-            //    }
-            //    con.Close();
-            //}
-            //catch(Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
-           
-
-
+            }                              
         }
     }
     
